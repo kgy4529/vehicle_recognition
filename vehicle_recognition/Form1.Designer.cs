8bc53d0dbc44c46568f3ac94529226f331a40c8a
kgy4529 <83329955+kgy4529@users.noreply.github.com>
2022-06-08 01:44:02 +0900

    이미지로드&판독 구현

---
+++ vehicle_recognition/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace vehicle_recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.loadbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(140, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 148);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(420, 175);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(75, 23);
+            this.btnClickThis.TabIndex = 1;
+            this.btnClickThis.Text = "인식";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            // 
+            // loadbtn
+            // 
+            this.loadbtn.Location = new System.Drawing.Point(420, 132);
+            this.loadbtn.Name = "loadbtn";
+            this.loadbtn.Size = new System.Drawing.Size(75, 23);
+            this.loadbtn.TabIndex = 2;
+            this.loadbtn.Text = "load";
+            this.loadbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loadbtn);
+            this.Controls.Add(this.btnClickThis);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Button loadbtn;
+    }
+}
+
---
+++ vehicle_recognition/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace vehicle_recognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
+            this.loadbtn.Click += new System.EventHandler(this.loadbtn_Click);
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("click!!");
+
+            //Bitmap img = new Bitmap(LoadBitmap(@"C:\Users\User\source\ex\kor.JPG"));
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            var ocr = new TesseractEngine("./tessdata", "kor", EngineMode.Default);
+            var texts = ocr.Process(img);
+            MessageBox.Show(texts.GetText());
+        }
+
+        private void loadbtn_Click(object sender, EventArgs e)
+        {
+            string image_file = string.Empty;
+
+            //파일 오픈창 생성 및 설정
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\";
+            dialog.Title = "이미지 파일 불러오기";
+            dialog.Filter = "그림 파일 (*.jpg, *.gif, *.bmp) | *.jpg; *.gif; *.bmp; | 모든 파일 (*.*) | *.*";
+            
+            //파일 오픈창 로드 및 ok 버튼 클릭
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image_file = dialog.FileName; //File경로와 File명을 모두 가지고 옴
+
+            }
+            else if(dialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            //pictureBox1.Image = Bitmap.FromFile(image_file);
+            Bitmap img = new Bitmap(LoadBitmap(image_file));
+            pictureBox1.Image = img;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        public Bitmap LoadBitmap(string file_name)
+        {
+            if (System.IO.File.Exists(file_name))
+            {
+                //파일을 읽기 전용으로 오픈
+                using (System.IO.FileStream stream= new System.IO.FileStream(file_name, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    //FileStream으로부터 BinaryReader를 구한다
+                    using (System.IO.BinaryReader reader = new System.IO.BinaryReader(stream))
+                    {
+                        //파일의 내용을 MemoryStream으로 복사
+                        var memoryStream = new System.IO.MemoryStream(reader.ReadBytes((int)stream.Length));
+                        //MemoryStream을 Bitmap으로 만들어 반환
+                        return new Bitmap(memoryStream);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("이미지를 찾을 수 없습니다", "이미지 로딩", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return null;
+            }
+        }
+    }
+}
